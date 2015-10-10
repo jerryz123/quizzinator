502d6ee90c319093f4802a28f0663e0aeca891ec
jerryz123 <qwertyuiopghb@gmail.com>
2015-10-10 08:47:39 -0700

    some working

--- serena.py
+++ serena.py
@@ -1,11 +1,125 @@
 from __future__ import unicode_literals, print_function
 from spacy.en import English
 from collections import Counter
+from spacy.parts_of_speech import *
 
 nlp = English()
 
-def hah():
-	text = nlp(input())
-	nounchunks = Counter([n.orth_.lower() for n in text.noun_chunks])
-	sentences = [s.orth_ for s in text.sents]
-	return nounchunks, sentences
+fil = open("sampletext.txt")
+t= fil.read().splitlines()
+test=''
+for i in t:
+	test+=i+" "
+
+class Question:
+	def __init__(self,sent,answer):
+		self.sentence=sent
+		self.answer=answer
+	def fillblank(self):
+		prompt=''
+		for w in self.sentence.abwords:
+			if w in self.answer:
+				prompt+="____ "
+			else:
+				prompt+=w.text+" "
+		return prompt
+
+class Sentence:
+	def __init__(self,span,parent,c=[]):
+		self.abstraction=span
+		self.abwords=[w for w in span]
+		self.text=span.text
+		self.paragraph=parent
+		self.makewords()
+		self.listchunks=[[w for w in chunk] for chunk in c]
+		self.questions=[Question(self,c) for c in self.listchunks]
+
+
+	def makewords(self):
+		d=[[w,Word(w,self,self.paragraph)] for w in self.abstraction]
+		for dup in d:
+			dup[1].sethead(listdic(d,dup[0].head))
+			for c in dup[0].children:
+				dup[1].addchild(listdic(d,c))
+		self.words=[dup[1] for dup in d]
+	def displayusage(self):
+		for w in self.words:
+			w.displayusage()
+	def display(self):
+		s=''
+		for w in self.words:
+			s+=w.text + " "
+		print(s)
+
+
+def listdic(lis,inp):
+	for dup in lis:
+		if dup[0]==inp:
+			return dup[1]
+
+
+class Paragraph:
+	def __init__(self,t):
+		self.abstraction=nlp(t)
+		self.listchunks()
+		self.sentences=[Sentence(s,self,self.findchunksinsentence(s)) for s in self.abstraction.sents]
+		
+		self.text=self.abstraction.text
+	def findchunkfreq(self):
+		chunks=[]
+		lis=[]
+		for chunk in self.abchunks:
+			if chunk not in chunks:
+				lis.append([chunk,self.abchunks.count(chunk)])
+				chunks.append(chunk)
+		lis.sort(key=(lambda x:x[1]))
+		self.sortedchunks=lis
+
+	def findchunksinsentence(self,sent):
+		chunks=[]
+		for c in self.abchunks:
+			if c.start>=sent.start and c.start<=sent.end:
+				chunks.append(c)
+		return chunks
+	def display(self): 
+		for s in self.sentences:
+			s.display()
+	def displayusage(self):
+		for s in self.sentences:
+			s.displayusage()
+	def listchunks(self):
+		self.abchunks=[c for c in self.abstraction.noun_chunks if len(c)>1]
+	def displaychunks(self):
+		
+
+def displayspan(span):
+	for w in span:
+		print(w.text,w.dep_)
+		
+
+
+
+
+class Word:
+	def __init__(self,token,sentence,paragraph):
+		self.text=token.text
+		self.abstraction=token
+		self.children=[]
+		self.usage=token.dep_
+	def display(self):
+		print(self.text)
+	def sethead(self,h):
+		self.head=h
+	def setchildren(self,ch=[]):
+		self.children=ch
+	def addchild(self,c):
+		self.children.append(c)
+	def displayusage(self):
+		print(self.text,self.usage)
+
+
+def listify(string):
+	text = nlp(string)
+	return[Sentence(s) for s in text.sents]
+
+
